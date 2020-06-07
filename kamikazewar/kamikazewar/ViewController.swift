307858276beb4624dbe51071f9884b9bd9023b1f
Jorge <jorgesantamariapoveda@gmail.com>
2020-06-07 09:10:45 +0200

    Crear proyecto

---
+++ kamikazewar/kamikazewar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kamikazewar
+//
+//  Created by Jorge on 07/06/2020.
+//  Copyright © 2020 Jorge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
